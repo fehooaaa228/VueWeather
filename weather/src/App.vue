5f541c136ebf19f5581a9287fd9b18f1050f2644
fehooaaa <dmarenkov2008@gmail.com>
2022-12-03 15:05:27 +0300

    [edit] firstComponent

---
+++ weather/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <firstComponent></firstComponent>
+</template>
+
+<script>
+import firstComponent from './components/firstComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    firstComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background: linear-gradient(to bottom right, #f279e6, #7b79f2) no-repeat;
+  height: 100%;
+  margin: 0px;
+}
+</style>
